8535993775209b1f7af3e3a959321163c320d526
fati <garibfati33@gmail.com>
2025-08-02 00:24:58 +0100

    commit avanc

--- geotnb_backend-master/src/parcelle/dto/update-parcelle.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateParcelleDto } from './create-parcelle.dto';
-
-export class UpdateParcelleDto extends PartialType(CreateParcelleDto) {}
